876ba03ef43ac5e844c7de9aa62797c495e22535
unknown <donutzue@gmail.com>
2018-04-24 16:48:05 +0700

    Test

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  graphqlUri: 'http://54.169.49.224:8082/graphql'
+  graphqlUri: 'http://54.71.80.147:8084/graphql'
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  graphqlUri: 'http://54.255.174.42:8082/graphql'
+  graphqlUri: 'http://54.71.80.147:8084/graphql'
 
 };
